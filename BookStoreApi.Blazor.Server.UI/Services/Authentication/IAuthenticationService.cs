959ce09cd0e8239b173b89803625610422daaf76
angel <fsguoangel@hotmail.com>
2022-07-21 20:54:36 +0800

    Create Blazor Server Client App

---
+++ BookStoreApi.Blazor.Server.UI/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStoreApi.Blazor.Server.UI.Services.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> AuthenticateAsync(LoginUserDto loginModel);
+        public Task Logout();
+    }
+}
